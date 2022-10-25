4c9ac7846d0e580b24e1c0904f658b694d317f2a
Carson Hartley <chartley1988@gmail.com>
2022-10-25 07:01:00 -0600

    Functionality is 90% done, just need to add new books with the Modal

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const library = document.getElementById("library")
-let addBookModal = false;
+let library = document.getElementById("library")
+let addBookModal = false; // Status whether modal is open or closed.
 
 const addBookButton = document.getElementById("addBookButton");
 addBookButton.addEventListener("click", toggleModal);
 const closeAddBookModal = document.getElementById("close-modal")
 closeAddBookModal.addEventListener("click", toggleModal);
 
-let myLibrary = [
-    {
-        title: "Dumb Book",
-        author: "Dumb Man",
-        page_count: "200",
-        read_status: "Unread"
-    },
-    {
-        title: "Siddhartha",
-        author: "Herman Hesse",
-        page_count: "200",
-        read_status: "Unread"
-    }
-];
+let SampleBook = new Book("Game of Thrones", "George RR Martin", 600, 'unread');
+let SampleBook1 = new Book("Feast of Crows", "George RR Martin", 600, 'unread');
+let SampleBook2 = new Book("Storm of Swords", "George RR Martin", 600, 'unread');
+let SampleBook3 = new Book("Clash of Kings", "George RR Martin", 600, 'unread');
+
+let myLibrary = [SampleBook, SampleBook1, SampleBook3, SampleBook2];
 
 function Book(title, author, page_count, read_status) { // Constructor
     this.title = title;
     this.author = author;
     this.page_count = page_count;
     this.read_status = read_status
+}
+
+Book.prototype.change_read_status = function() {
+    if (this.read_status === 'read') {
+        this.read_status = 'unread';
+    } else {
+        this.read_status = 'read';
+    }
+    return this.read_status;
 }
 
 /* function addBookToLibrary(title, author, page_count, read_status) {
@@ -40,52 +41,97 @@ function Book(title, author, page_count, read_status) { // Constructor
 
 
 function displayLibrary () {
-    myLibrary.forEach(element => {
-        const new_book = buildCard(element);
+    library.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        const new_book = buildCard(myLibrary[i], i)
+        new_book.setAttribute('data-index', i)
         library.appendChild(new_book);
-    });
+    }
 }
 
 
-function buildCard (book) {
+function buildCard (book, index) {
     // Create library book card and add CSS class to it
     const card = document.createElement('div');
     card.classList.add("library-listing");
+
     // Create Title info
     const card_title = document.createElement('p');
     card_title.textContent = ("Title: " + book.title);
     card.appendChild(card_title);
+
         // Create Author info
     const card_author = document.createElement('p');
     card_author.textContent = ("Author: " + book.author);
     card.appendChild(card_author);
+
     // Create Page info
     const card_page_count = document.createElement('p');
     card_page_count.textContent = ("Page Count: " + book.page_count);
     card.appendChild(card_page_count);
+
     // Create Read Status info
     const card_read_status = document.createElement('p');
     card_read_status.textContent = ("Read Status: " + book.read_status);
     card.appendChild(card_read_status);
 
+    // Create button container for 'read'/'unread' and 'Remove Book'
+    const button_wrapper = document.createElement('div');
+    button_wrapper.style.cssText = 'display: flex; flex-direction: row; justify-content: center; gap: 1rem;';
+    card.appendChild(button_wrapper);
+    const toggleRead = document.createElement('button');
+    toggleRead.classList.add('modal-freeze');
+    toggleRead.textContent = 'Change Read Status';
+    toggleRead.addEventListener('click', () => {
+        const BookObject = myLibrary[index];
+        const StatusText = BookObject.change_read_status();
+        card_read_status.textContent = ('Read Status: ' + StatusText);
+    });
+        
+    const removeBook = document.createElement('button');
+    removeBook.classList.add('modal-freeze');
+    removeBook.textContent = 'Remove Book';
+    removeBook.addEventListener('click', () => {
+        myLibrary.shift(index);
+        console.log(library);
+        console.log(myLibrary);
+        displayLibrary();
+    });
+    
+    button_wrapper.appendChild(toggleRead);
+    button_wrapper.appendChild(removeBook);
+
     return(card);
 }
 
+
 function toggleModal() {
     const modal = document.getElementById("add-book");
-    console.log(modal);
     if (addBookModal === false) {
         addBookButton.disabled = true;
-        addBookButton.classList.add("disabled");
         modal.style.display = 'block';
         addBookModal = true;
+        freezePage();
     } else {
         modal.style.display = 'none';
         addBookModal = false;
         addBookButton.disabled = false;
-        addBookButton.classList.remove("disabled");
+        freezePage();
     }
-    console.log(addBookModal);
 }
+
+function freezePage() { // Freezes all buttons on the page when modal is open.
+    const buttonList = document.getElementsByClassName('modal-freeze');
+    const buttonArray = Array.from(buttonList);
+    if (addBookModal === true) {
+        buttonArray.forEach(element => {
+            element.classList.add('disabled');
+        });
+    } else {
+        buttonArray.forEach(element => {
+            element.classList.remove('disabled');
+        });
+    }
+}  
 
 displayLibrary();
